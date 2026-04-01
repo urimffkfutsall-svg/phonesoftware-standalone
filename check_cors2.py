3afe184e387bbe011ef40d79f8b16b876659f62a
urimffkfutsall-svg <urim.ffkfutsall@gmail.com>
2026-04-01 18:49:39 +0200

    fix: CORS wildcard + form redesign

---
+++ check_cors2.py
@@ -0,0 +1,4 @@
+﻿with open('backend/server.py', 'r', encoding='utf-8') as f:
+    c = f.read()
+idx = c.find('add_middleware')
+print(c[idx-5:idx+300])
---
+++ check_cors_now.py
@@ -0,0 +1,4 @@
+﻿with open('backend/server.py', 'r', encoding='utf-8') as f:
+    c = f.read()
+idx = c.find('CORSMiddleware')
+print(c[idx-10:idx+300])
