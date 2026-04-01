7fc9258b09c0dc7fba1c26c5f56f8a1f893baa2d
urimffkfutsall-svg <urim.ffkfutsall@gmail.com>
2026-04-01 17:17:34 +0200

    fix: CORS credentials conflict with wildcard origin

---
+++ check_cors.py
@@ -0,0 +1,3 @@
+﻿with open('backend/server.py', 'r', encoding='utf-8') as f:
+    c = f.read()
+print(c[:2000])
---
+++ fix_cors.py
@@ -0,0 +1,28 @@
+﻿with open('backend/server.py', 'r', encoding='utf-8') as f:
+    c = f.read()
+
+old = '''app.add_middleware(
+CORSMiddleware,
+allow_origins=["*"],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)'''
+
+new = '''app.add_middleware(
+CORSMiddleware,
+allow_origins=[
+    "https://phonesoftware-frontend.onrender.com",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "*"
+],
+allow_credentials=False,
+allow_methods=["*"],
+allow_headers=["*"],
+)'''
+
+c = c.replace(old, new)
+with open('backend/server.py', 'w', encoding='utf-8') as f:
+    f.write(c)
+print('server.py fixed!')
